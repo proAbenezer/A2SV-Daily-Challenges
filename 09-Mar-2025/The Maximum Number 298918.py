a5bfe147ca04a86c41f6325bbdd93fc3fa63abfc
Abenezer Haile <151909961+proAbenezer@users.noreply.github.com>
2025-03-09 22:04:50 +0300

    Created 'The Maximum Number' by A2SV Hub

---
+++ 09-Mar-2025/The Maximum Number 298918.py
@@ -0,0 +1,8 @@
+# Problem: The Maximum Number - https://leetcode.com/problems/third-maximum-number/description/
+
+class Solution:
+    from collections import Counter
+    def thirdMax(self, nums: List[int]) -> int:
+        nums = sorted(set(nums), reverse=True)
+        if len(nums) < 3: return max(nums)
+        return nums[2]
