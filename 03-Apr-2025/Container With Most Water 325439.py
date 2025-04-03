488cdd92f475b0a30e6c64baa23e08eb089ba3de
Abenezer Haile <151909961+proAbenezer@users.noreply.github.com>
2025-04-03 22:25:28 +0300

    Created 'Container With Most Water' by A2SV Hub

---
+++ 03-Apr-2025/Container With Most Water 325439.py
@@ -0,0 +1,14 @@
+# Problem: Container With Most Water - https://leetcode.com/problems/container-with-most-water/
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left, right = 0, len(height) - 1
+        result = 0
+        while left < right:
+            area = (right - left) * min(height[left], height[right])
+            result = max(result, area)
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return result
