152c95a2480953ba8c9babd142806c4350f18438
Abenezer Haile <151909961+proAbenezer@users.noreply.github.com>
2025-04-05 22:56:08 +0300

    Created 'Fruit Into Baskets' by A2SV Hub

---
+++ 05-Apr-2025/Fruit Into Baskets 327031.py
@@ -0,0 +1,20 @@
+# Problem: Fruit Into Baskets - https://leetcode.com/problems/fruit-into-baskets
+
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        count = collections.defaultdict(int)
+        res, total, l = 0, 0, 0
+        
+        for r in range(len(fruits)):
+            total += 1
+            count[fruits[r]] += 1
+            
+            while len(count) > 2:
+                f = fruits[l]
+                total -= 1
+                count[f] -= 1
+
+                if count[f] == 0: count.pop(f)
+                l += 1
+            res = max(res, total)
+        return res
