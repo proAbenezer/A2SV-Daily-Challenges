75708b5763f71f5a93bb539071133e7220fba9fc
Abenezer Haile <151909961+proAbenezer@users.noreply.github.com>
2025-05-04 22:07:48 +0300

    Created 'Tower of Hanoi ' by A2SV Hub

---
+++ 04-May-2025/Tower of Hanoi  348190.py
@@ -0,0 +1,13 @@
+# Problem: Tower of Hanoi  - https://www.geeksforgeeks.org/c-program-for-tower-of-hanoi/
+
+def TowerOfHanoi(n, from_rod, to_rod, aux_rod):
+    if n == 0:
+        return
+    TowerOfHanoi(n-1, from_rod, aux_rod, to_rod)
+    print("Move disk", n, "from rod", from_rod, "to rod", to_rod)
+    TowerOfHanoi(n-1, aux_rod, to_rod, from_rod)
+
+
+N = 3
+
+TowerOfHanoi(N, 'A', 'C', 'B')
